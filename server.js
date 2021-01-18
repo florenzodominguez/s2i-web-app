898f28f84892e041c662e1f88b693991f5592dbe
florenzodominguez <71065752+florenzodominguez@users.noreply.github.com>
2021-01-18 12:56:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
 'use strict';
-const winston = require('winston');
-const { ElasticsearchTransport } = require('winston-elasticsearch');
 
-const esTransportOpts = {
-  level: 'info'
-};
-const esTransport = new ElasticsearchTransport(esTransportOpts);
-const logger = winston.createLogger({
-  transports: [
-    esTransport
-  ]
-});
 // Compulsory error handling
 logger.on('error', (error) => {
   console.error('Error caught', error);
